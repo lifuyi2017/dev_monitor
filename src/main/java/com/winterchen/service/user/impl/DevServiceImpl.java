95bff08efd3437ffef744c7c24ce7fe52d002553
W9007579 <1550425634@qq.com>
2020-09-27 18:32:18 +0800

    2020-09-27

---
+++ src/main/java/com/winterchen/service/user/impl/DevServiceImpl.java
@@ -0,0 +1,8 @@
+package com.winterchen.service.user.impl;
+
+import com.winterchen.service.user.DevService;
+import org.springframework.stereotype.Service;
+
+@Service(value = "devService")
+public class DevServiceImpl implements DevService {
+}
