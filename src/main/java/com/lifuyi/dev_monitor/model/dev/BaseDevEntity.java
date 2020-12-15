f34a837d7d4f4715afe79114041d2f77f03d811d
W9007579 <1550425634@qq.com>
2020-12-15 19:00:24 +0800

    企业管理

--- src/main/java/com/lifuyi/dev_monitor/model/dev/BaseDevEntity.java
+++ src/main/java/com/lifuyi/dev_monitor/model/dev/BaseDevEntity.java
@@ -3,5 +3,8 @@ package com.lifuyi.dev_monitor.model.dev;
 public class BaseDevEntity {
 
     private String dev_code;
+    private String dev_type_id;
+    private String dev_name;
+    private String
 
 }
