ccd1b26fb51e4a1965ccca0fd023ada688fc5dc9
W9007579 <1550425634@qq.com>
2020-12-30 18:36:13 +0800

    12-30

--- src/main/java/com/lifuyi/dev_monitor/model/enterprise/Resp/EnterpriseResp.java
+++ src/main/java/com/lifuyi/dev_monitor/model/enterprise/Resp/EnterpriseResp.java
@@ -1,16 +1,26 @@
 package com.lifuyi.dev_monitor.model.enterprise.Resp;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @Data
+@Api(value = "企业查询结果实体")
 public class EnterpriseResp {
+    @ApiModelProperty(value = "id" ,position = 1)
     private String enterprise_id;
+    @ApiModelProperty(value = "企业编码" ,position = 2)
     private String enterprise_code;
+    @ApiModelProperty(value = "企业名称" ,position = 3)
     private String enterprise_name;
+    @ApiModelProperty(value = "企业类型" ,position = 4)
     private EnterpriseTypeResp enterpriseTypeResp;
+    @ApiModelProperty(value = "更新时间" ,position = 5)
     private Date update_time;
-    private List<String> service_names;
+    @ApiModelProperty(value = "banding的企业" ,position = 5)
+    private Map<String,String> service_names;
 }
